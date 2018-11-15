a079527818fd0f01fa6a99db8961133fa38e32d7
xingdong <xingdong1117@126.com>
2018-11-15 11:11:30 +0800

    2018-11-15

---
+++ basic/loop.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func convertToBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	printFileContents(file)
+
+}
+
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	fmt.Println(convertToBin(5))  //101
+	fmt.Println(convertToBin(13)) //1101
+
+	printFile("abc.txt")
+
+	s := `adv"d"
+		kk
+			111
+		ddd
+	`
+
+	printFileContents(strings.NewReader(s))
+
+}
